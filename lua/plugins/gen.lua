8b076119ffd3fab413fbbf4580129457cacc6299
Charles Ueltschey <me@cueltschey.com>
2025-11-30 13:09:49 -0600

    Add gen AI package

---
+++ lua/plugins/gen.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "tinyllama",
+      quit_map = "q",
+      retry_map = "<c-r>",
+      accept_map = "<c-cr>",
+      host = "localhost",
+      port = "11434",
+      display_mode = "float",
+      show_prompt = false,
+      show_model = true,
+      no_auto_close = false,
+      file = false,
+      hidden = false,
+      init = function(options)
+        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+      end,
+      command = function(options)
+        local body = { model = options.model, stream = true }
+        return "curl --silent --no-buffer -X POST http://"
+          .. options.host
+          .. ":"
+          .. options.port
+          .. "/api/chat -d $body"
+      end,
+      result_filetype = "markdown",
+      debug = false,
+    },
+  },
+}
